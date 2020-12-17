1a68483c2f38e9b2c5bc0597f4dd8edabf20a097
Valdemir Filho <filhux@gmail.com>
2020-12-17 05:58:48 -0300

    Adiciona novo layout padrão

---
+++ template/js/script.js
@@ -0,0 +1,7 @@
+
+  function showMenu() {
+    document.getElementById('player').play();
+    document.querySelector('#modal').style.display = "none";
+    document.querySelector('body').style.overflow = "auto";
+  }
+
