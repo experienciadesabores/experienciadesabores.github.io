4940ba56fcbcf99e9630f6e6ae66f5e011ee0f69
Valdemir Filho <filhux@gmail.com>
2020-12-19 03:48:11 -0300

    Adiciona menu05 e redirect

---
+++ menu05/js/script.js
@@ -0,0 +1,6 @@
+function showMenu() {
+  document.getElementById('player').play();
+  document.querySelector('#modal').style.display = "none";
+  document.querySelector('body').style.overflow = "auto";
+}
+
